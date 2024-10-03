d89bb74e83ab2b4625d51da37528716b3fc5c4b0
MikePetrosyan <michael.petrosyan@outlook.com>
2024-10-03 17:30:48 -0400

    backend.redux and blogscreen setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BlogScreen from './screens/BlogScreen';
+
 const App = () => {
   return (
     <ChakraProvider>
       <Router>
         <Navbar />
         <main>
-          <Routes></Routes>
+          <Routes>
+            <Route exact path='/blog/:category' element={<BlogScreen />}></Route>
+          </Routes>
         </main>
       </Router>
     </ChakraProvider>
